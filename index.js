b2420d5072d98eea5dd6be9478e0ed5a01a6f105
itsmarsss <itsmarzzzzzz@gmail.com>
2023-06-12 18:11:52 -0400

    Scale scroll length by visible content height

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const gen = document.getElementById("cons-gen");
+const bio = document.getElementById("cons-bio");
+const chem = document.getElementById("cons-chem");
+const phys = document.getElementById("cons-phys");
+
 function toggleVid() {
     document.getElementById("video").classList.toggle("hidden-img");
 
@@ -12,4 +17,21 @@ function toggleVid() {
 function slide(amt) {
     const pctg = amt * 25;
     document.getElementById("section-container").style.transform = `translateX(-${pctg}%)`
+
+    setTimeout(function () {
+        gen.style.display = "none";
+        bio.style.display = "none";
+        chem.style.display = "none";
+        phys.style.display = "none";
+
+        if (amt == 0) {
+            gen.style.display = "flex";
+        } else if (amt == 1) {
+            bio.style.display = "flex";
+        } else if (amt == 2) {
+            chem.style.display = "flex";
+        } else if (amt == 3) {
+            phys.style.display = "flex";
+        }
+    }, 200);
 }
